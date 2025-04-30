41155a1e65c475bce811660c7fc2c802f41d53df
Sujintra Frazier <sujintra.frazier@wsu.edu>
2025-04-29 22:56:21 -0700

    Created test for invalid weight input. This ensures that the proper error message appears when the users enter an invalid value in the 'Exercise weight' field.

--- MyFitnessApp/Tests/MyFitnessApp.Web.Tests/SeleniumExerciseDiaryTests.cs
+++ MyFitnessApp/Tests/MyFitnessApp.Web.Tests/SeleniumExerciseDiaryTests.cs
@@ -19,7 +19,7 @@ public class ExerciseDiaryTests : IDisposable
     }
 
     [Fact]
-    public void TestExerciseDiary()
+    public void TestValidExerciseDiary()
     {
         this._driver.Navigate().GoToUrl(this._url + "Identity/Account/Login");
 
@@ -59,6 +59,49 @@ public class ExerciseDiaryTests : IDisposable
         Thread.Sleep(5000);
     }
 
+    [Fact]
+    public void TestInvalidWeights()
+    {
+        this._driver.Navigate().GoToUrl(this._url + "Identity/Account/Login");
+
+        var usernameField = this._driver.FindElement(By.Id("Input_Username"));
+        var passwordField = this._driver.FindElement(By.Id("Input_Password"));
+
+        this.SimulateTyping(usernameField, "testuser");
+        this.SimulateTyping(passwordField, "123456");
+
+        var loginButton = this._driver.FindElement(By.CssSelector("button[type='submit']"));
+        loginButton.Click();
+
+        this._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/All");
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/Add/59");
+
+        var wait = new WebDriverWait(this._driver, TimeSpan.FromSeconds(10));
+
+        var weightField = wait.Until(d => d.FindElement(By.Id("Weight")));
+        weightField.Clear();
+        weightField.SendKeys("-100");
+
+        var repetitionsField = wait.Until(d => d.FindElement(By.Id("Repetitions")));
+        repetitionsField.Clear();
+        repetitionsField.SendKeys("5");
+
+        var setsField = wait.Until(d => d.FindElement(By.Id("Sets")));
+        setsField.Clear();
+        setsField.SendKeys("3");
+
+        var weekdaySelect = new SelectElement(this._driver.FindElement(By.Id("WeekDay")));
+        weekdaySelect.SelectByValue("1");
+
+        this._driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+        var weightErrorMessage = wait.Until(d => d.FindElement(By.CssSelector("span[data-valmsg-for='Weight']"))).Text;
+
+        Assert.Equal("The value must be between 0 and 500", weightErrorMessage);
+
+        Thread.Sleep(5000);
+    }
     private void SimulateTyping(IWebElement element, string text)
     {
         foreach (char character in text)
