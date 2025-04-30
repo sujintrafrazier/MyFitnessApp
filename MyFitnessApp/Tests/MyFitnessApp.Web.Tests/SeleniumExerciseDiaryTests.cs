77fc341ab98ca3e9b8575fbd9298ecbfae901e3c
Sujintra Frazier <sujintra.frazier@wsu.edu>
2025-04-29 23:27:22 -0700

    For the exercise diary tests, the login logic was moved into the constructor to reduce redundancy for each test case. Created test cases for invalid repetitions and invalid sets. This ensures that the proper error message is displayed to the screen when users enters an invalid input.

--- MyFitnessApp/Tests/MyFitnessApp.Web.Tests/SeleniumExerciseDiaryTests.cs
+++ MyFitnessApp/Tests/MyFitnessApp.Web.Tests/SeleniumExerciseDiaryTests.cs
@@ -16,13 +16,8 @@ public class ExerciseDiaryTests : IDisposable
         options.AddArgument("--headless");
         this._driver = new ChromeDriver();
         this._url = "https://localhost:5001/";
-    }
 
-    [Fact]
-    public void TestValidExerciseDiary()
-    {
         this._driver.Navigate().GoToUrl(this._url + "Identity/Account/Login");
-
         var usernameField = this._driver.FindElement(By.Id("Input_Username"));
         var passwordField = this._driver.FindElement(By.Id("Input_Password"));
 
@@ -31,7 +26,11 @@ public class ExerciseDiaryTests : IDisposable
 
         var loginButton = this._driver.FindElement(By.CssSelector("button[type='submit']"));
         loginButton.Click();
+    }
 
+    [Fact]
+    public void TestValidExerciseDiary()
+    {
         this._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         this._driver.Navigate().GoToUrl(this._url + "Exercises/All");
         this._driver.Navigate().GoToUrl(this._url + "Exercises/Add/60");
@@ -62,17 +61,6 @@ public class ExerciseDiaryTests : IDisposable
     [Fact]
     public void TestInvalidWeights()
     {
-        this._driver.Navigate().GoToUrl(this._url + "Identity/Account/Login");
-
-        var usernameField = this._driver.FindElement(By.Id("Input_Username"));
-        var passwordField = this._driver.FindElement(By.Id("Input_Password"));
-
-        this.SimulateTyping(usernameField, "testuser");
-        this.SimulateTyping(passwordField, "123456");
-
-        var loginButton = this._driver.FindElement(By.CssSelector("button[type='submit']"));
-        loginButton.Click();
-
         this._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         this._driver.Navigate().GoToUrl(this._url + "Exercises/All");
         this._driver.Navigate().GoToUrl(this._url + "Exercises/Add/59");
@@ -102,6 +90,73 @@ public class ExerciseDiaryTests : IDisposable
 
         Thread.Sleep(5000);
     }
+
+    [Fact]
+    public void TestInvalidRepetitions()
+    {
+        this._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/All");
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/Add/58");
+
+        var wait = new WebDriverWait(this._driver, TimeSpan.FromSeconds(10));
+
+        var weightField = wait.Until(d => d.FindElement(By.Id("Weight")));
+        weightField.Clear();
+        weightField.SendKeys("100");
+
+        var repetitionsField = wait.Until(d => d.FindElement(By.Id("Repetitions")));
+        repetitionsField.Clear();
+        repetitionsField.SendKeys("-5");
+
+        var setsField = wait.Until(d => d.FindElement(By.Id("Sets")));
+        setsField.Clear();
+        setsField.SendKeys("3");
+
+        var weekdaySelect = new SelectElement(this._driver.FindElement(By.Id("WeekDay")));
+        weekdaySelect.SelectByValue("1");
+
+        this._driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+        var repsErrorMessage = wait.Until(d => d.FindElement(By.CssSelector("span[data-valmsg-for='Repetitions']"))).Text;
+
+        Assert.Equal("The value must be between 1 and 100", repsErrorMessage);
+
+        Thread.Sleep(5000);
+    }
+
+    [Fact]
+    public void TestInvalidSets()
+    {
+        this._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/All");
+        this._driver.Navigate().GoToUrl(this._url + "Exercises/Add/57");
+
+        var wait = new WebDriverWait(this._driver, TimeSpan.FromSeconds(10));
+
+        var weightField = wait.Until(d => d.FindElement(By.Id("Weight")));
+        weightField.Clear();
+        weightField.SendKeys("100");
+
+        var repetitionsField = wait.Until(d => d.FindElement(By.Id("Repetitions")));
+        repetitionsField.Clear();
+        repetitionsField.SendKeys("5");
+
+        var setsField = wait.Until(d => d.FindElement(By.Id("Sets")));
+        setsField.Clear();
+        setsField.SendKeys("0");
+
+        var weekdaySelect = new SelectElement(this._driver.FindElement(By.Id("WeekDay")));
+        weekdaySelect.SelectByValue("1");
+
+        this._driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+        var setsErrorMessage = wait.Until(d => d.FindElement(By.CssSelector("span[data-valmsg-for='Sets']"))).Text;
+
+        Assert.Equal("The value must be between 1 and 100", setsErrorMessage);
+
+        Thread.Sleep(5000);
+    }
+
     private void SimulateTyping(IWebElement element, string text)
     {
         foreach (char character in text)
